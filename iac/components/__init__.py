a14164f281e0a95028f1d033030406c1103c92b6
Ignatus <ignatusa3@gmail.com>
2025-07-31 10:13:36 +0000

    chore: deploy ec2 instance

--- iac/components/__init__.py
+++ iac/components/__init__.py
@@ -2,8 +2,7 @@
 
 This package groups all custom ComponentResources so they can be imported as:
 
-    from iac.components import Vpc, StorageBucket, ComputeInstance
+    from iac.components import Vpc, ComputeInstance
 """
 from .vpc import Vpc  # noqa: F401
-from .s3 import StorageBucket  # noqa: F401
 from .ec2 import ComputeInstance  # noqa: F401
